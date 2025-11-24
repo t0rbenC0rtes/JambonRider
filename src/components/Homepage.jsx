79291a157f8090677c7b185aaa145e2231633dc2
Antoine L <162304112+t0rbenC0rtes@users.noreply.github.com>
2025-11-24 19:53:24 +0100

    Fix image source case sensitivity in Homepage component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
     <div className="homepage">
       <div className="homepage-content">
         <img 
-          src="/img/Jambon.jpg" 
+          src="/img/jambon.jpg" 
           alt="JambonRider Logo" 
           className="homepage-logo"
         />
